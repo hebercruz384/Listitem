97cc1523f54fcb1f9ad3db06c3075115d5059097
hebercruz384 <73669492+hebercruz384@users.noreply.github.com>
2021-10-25 18:31:56 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+function listItem(firstName, lastName) {
+  return (
+    <li>
+      {firstName} {lastName}
+    </li> 
+{
+  return (
+    <div className="App">
+      {Listitem ("Heber","Cruz")}
+  );
+}
